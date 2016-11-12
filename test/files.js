04548389150f208da900623a1c7b438e0864f4df
Mark Wubben <mark@novemberborn.net>
2016-11-12 14:18:25 +0000

    Test plugin effect, not generated code

--- test/files.js
+++ test/files.js
@@ -1,4 +1,5 @@
 import { join } from 'path'
+import { runInNewContext } from 'vm'
 
 import test from 'ava'
 import { transform as babelTransform } from 'babel-core'
@@ -34,6 +35,12 @@ function check (msg, sourceRoot = __dirname) {
   }
 }
 
+const compareResult = (t, { code, sourceRoot, plugin, expected }) => {
+  const actual = runInNewContext(transform(code, sourceRoot, plugin))
+  t.deepEqual(actual, expected)
+  t.true(Object.isFrozen(actual))
+}
+
 test('throws when importing members', t => {
   t.throws(
     attempt("import { foo } from 'files:fixtures/*'"),
@@ -64,117 +71,106 @@ test('throws if pattern is absolute', t => {
     check("Glob pattern must be relative, was '/root'"))
 })
 
-test('generates an object with descriptions of the matched files', t => {
-  t.is(
-    transform("import foo from 'files:fixtures/rfc3092.*'"),
-    `const foo = {
-  'rfc3092.html': {
-    contentType: 'text/html; charset=utf-8',
-    mediaType: 'text/html',
-    size: 44585,
-    src: 'test/fixtures/rfc3092.html',
-    tag: '1b8c719d6a9c0398b7b9b3ff85763413'
-  },
-  'rfc3092.pdf': {
-    contentType: 'application/pdf',
-    mediaType: 'application/pdf',
-    size: 38334,
-    src: 'test/fixtures/rfc3092.pdf',
-    tag: '5459e23f9445a65c2bf61eeac5882852'
-  },
-  'rfc3092.txt': {
-    contentType: 'text/plain; charset=utf-8',
-    mediaType: 'text/plain',
-    size: 29235,
-    src: 'test/fixtures/rfc3092.txt',
-    tag: 'a9a1b44ecd667818a0c21737bfb0102d'
+test('generates an object with descriptions of the matched files', compareResult, {
+  code: "import foo from 'files:fixtures/rfc3092.*'; foo",
+  expected: {
+    'rfc3092.html': {
+      contentType: 'text/html; charset=utf-8',
+      mediaType: 'text/html',
+      size: 44585,
+      src: 'test/fixtures/rfc3092.html',
+      tag: '1b8c719d6a9c0398b7b9b3ff85763413'
+    },
+    'rfc3092.pdf': {
+      contentType: 'application/pdf',
+      mediaType: 'application/pdf',
+      size: 38334,
+      src: 'test/fixtures/rfc3092.pdf',
+      tag: '5459e23f9445a65c2bf61eeac5882852'
+    },
+    'rfc3092.txt': {
+      contentType: 'text/plain; charset=utf-8',
+      mediaType: 'text/plain',
+      size: 29235,
+      src: 'test/fixtures/rfc3092.txt',
+      tag: 'a9a1b44ecd667818a0c21737bfb0102d'
+    }
   }
-};
-Object.freeze(foo);`)
 })
 
-test('ignores matched directories', t => {
-  t.is(
-    transform("import foo from 'files:fixtures/!(*.html|*.pdf|with-package)'"),
-    `const foo = {
-  'rfc3092.txt': {
-    contentType: 'text/plain; charset=utf-8',
-    mediaType: 'text/plain',
-    size: 29235,
-    src: 'test/fixtures/rfc3092.txt',
-    tag: 'a9a1b44ecd667818a0c21737bfb0102d'
+test('ignores matched directories', compareResult, {
+  code: "import foo from 'files:fixtures/!(*.html|*.pdf|with-package)'; foo",
+  expected: {
+    'rfc3092.txt': {
+      contentType: 'text/plain; charset=utf-8',
+      mediaType: 'text/plain',
+      size: 29235,
+      src: 'test/fixtures/rfc3092.txt',
+      tag: 'a9a1b44ecd667818a0c21737bfb0102d'
+    }
   }
-};
-Object.freeze(foo);`)
 })
 
-test('object keys are the file paths without the common path prefix', t => {
-  t.is(
-    transform("import foo from 'files:fixtures/{*.txt,nested/*.txt}'"),
-    `const foo = {
-  'nested/foo.txt': {
-    contentType: 'text/plain; charset=utf-8',
-    mediaType: 'text/plain',
-    size: 0,
-    src: 'test/fixtures/nested/foo.txt',
-    tag: 'd41d8cd98f00b204e9800998ecf8427e'
-  },
-  'rfc3092.txt': {
-    contentType: 'text/plain; charset=utf-8',
-    mediaType: 'text/plain',
-    size: 29235,
-    src: 'test/fixtures/rfc3092.txt',
-    tag: 'a9a1b44ecd667818a0c21737bfb0102d'
+test('object keys are the file paths without the common path prefix', compareResult, {
+  code: "import foo from 'files:fixtures/{*.txt,nested/*.txt}'; foo",
+  expected: {
+    'nested/foo.txt': {
+      contentType: 'text/plain; charset=utf-8',
+      mediaType: 'text/plain',
+      size: 0,
+      src: 'test/fixtures/nested/foo.txt',
+      tag: 'd41d8cd98f00b204e9800998ecf8427e'
+    },
+    'rfc3092.txt': {
+      contentType: 'text/plain; charset=utf-8',
+      mediaType: 'text/plain',
+      size: 29235,
+      src: 'test/fixtures/rfc3092.txt',
+      tag: 'a9a1b44ecd667818a0c21737bfb0102d'
+    }
   }
-};
-Object.freeze(foo);`)
 })
 
-test('file src is relative to the closest package.json', t => {
-  const sourceRoot = join(__dirname, 'fixtures', 'with-package')
-  t.is(
-    transform("import foo from 'files:*.txt'", sourceRoot),
-    `const foo = {
-  'foo.txt': {
-    contentType: 'text/plain; charset=utf-8',
-    mediaType: 'text/plain',
-    size: 0,
-    src: 'foo.txt',
-    tag: 'd41d8cd98f00b204e9800998ecf8427e'
+test('file src is relative to the closest package.json', compareResult, {
+  code: "import foo from 'files:*.txt'; foo",
+  sourceRoot: join(__dirname, 'fixtures', 'with-package'),
+  expected: {
+    'foo.txt': {
+      contentType: 'text/plain; charset=utf-8',
+      mediaType: 'text/plain',
+      size: 0,
+      src: 'foo.txt',
+      tag: 'd41d8cd98f00b204e9800998ecf8427e'
+    }
   }
-};
-Object.freeze(foo);`)
 })
 
-test('file src is relative to working directory if there is no closest package.json', t => {
-  const sourceRoot = join(__dirname, 'fixtures', 'with-package')
-  t.is(
-    transform("import foo from 'files:*.txt'", sourceRoot, pluginWithoutPkgDir),
-    `const foo = {
-  'foo.txt': {
-    contentType: 'text/plain; charset=utf-8',
-    mediaType: 'text/plain',
-    size: 0,
-    src: 'fixtures/with-package/foo.txt',
-    tag: 'd41d8cd98f00b204e9800998ecf8427e'
+test('file src is relative to working directory if there is no closest package.json', compareResult, {
+  code: "import foo from 'files:*.txt'; foo",
+  sourceRoot: join(__dirname, 'fixtures', 'with-package'),
+  plugin: pluginWithoutPkgDir,
+  expected: {
+    'foo.txt': {
+      contentType: 'text/plain; charset=utf-8',
+      mediaType: 'text/plain',
+      size: 0,
+      src: 'fixtures/with-package/foo.txt',
+      tag: 'd41d8cd98f00b204e9800998ecf8427e'
+    }
   }
-};
-Object.freeze(foo);`)
 })
 
-test('defaults to application/octet-stream for unknown file types', t => {
-  t.is(
-    transform("import foo from 'files:fixtures/unknown/*'"),
-    `const foo = {
-  'foo.\\uD83D\\uDCA9': {
-    contentType: 'application/octet-stream',
-    mediaType: 'application/octet-stream',
-    size: 5,
-    src: 'test/fixtures/unknown/foo.\\uD83D\\uDCA9',
-    tag: '69f8a61d4ae5157ea81fd82ec0e777a9'
+test('defaults to application/octet-stream for unknown file types', compareResult, {
+  code: "import foo from 'files:fixtures/unknown/*'; foo",
+  expected: {
+    'foo.💩': {
+      contentType: 'application/octet-stream',
+      mediaType: 'application/octet-stream',
+      size: 5,
+      src: 'test/fixtures/unknown/foo.💩',
+      tag: '69f8a61d4ae5157ea81fd82ec0e777a9'
+    }
   }
-};
-Object.freeze(foo);`)
 })
 
 test('ignores unrelated imports', t => {
